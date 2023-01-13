dfc1c752baffc3005deaabcbe26c278bbfb4cb73
florentazd <florent4real@yahoo.com>
2023-01-13 10:20:41 +0100

    add text to the root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-Page de présentation du link shorter
+<ul class="utils-links text-blue-500 px-4 sm:px-40" >
+    <br><br>
+    <h1 class="text-3xl font-semibold text-black">Liens Utils</h1>
+    <li><a href="/login">Connexion</a></li>
+    <li><a href="/register">inscription</a></li>
+    <li><a href="/dashboard">Dashboard</a></li>
+</ul>
